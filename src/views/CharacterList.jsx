39b6f271a692ba01ecd2f6d16c251448ca75f7fb
Andrea Cleland <andreaclelan@gmail.com>
2022-05-09 10:46:53 -0700

    now displays list of characters

--- src/views/CharacterList.jsx
+++ src/views/CharacterList.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function CharacterList() {
-  return <div>CharacterList</div>;
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((res) => res.json())
+      .then((res) => setCharacters(res.results))
+      .then(() => setLoading(false))
+      .catch((err) => console.log('oh no error', err));
+  }, []);
+
+  return (
+    <>
+      <h1>Rick and Morty Characters</h1>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.id}>
+            <span>
+              <img
+                width="100px"
+                src={character.image}
+                alt={`Image of ${character.name}`}
+              />
+              <h3>{character.name}</h3>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
